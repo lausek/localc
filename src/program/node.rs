e1b55e6a757691849581ba69805e67be0ef01de7
lausek <root@lausek.eu>
2018-10-22 17:22:58 +0200

    renamed assignment node

--- src/program/node.rs
+++ src/program/node.rs
@@ -33,7 +33,7 @@ impl std::fmt::Display for Node
             Mul(x, y) => write!(f, "{} * {}", x, y),
             Div(x, y) => write!(f, "{} / {}", x, y),
             Pow(x, y) => write!(f, "{}^({})", x, y),
-            Mov(x, y) => write!(f, "{} = {}", x, y),
+            Mov(x, y) => write!(f, "{} := {}", x, y),
             Val(x)    => write!(f, "{}", x),
             Var(x)    => write!(f, "{}", x),
             Func(x, y) => {

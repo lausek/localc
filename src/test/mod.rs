c78fe46395e6ba1405fa7dc10039921c756658e9
lausek <root@lausek.eu>
2019-05-18 19:06:09 +0200

    added tests

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -28,7 +28,7 @@ macro_rules! expect {
 }
 
 #[test]
-fn building_fib() {
+fn fib() {
     let mut repl = Repl::new();
 
     repl.run("f(0) = 0").unwrap();
@@ -41,7 +41,7 @@ fn building_fib() {
 }
 
 #[test]
-fn building_sqrt() {
+fn sqrt() {
     let mut repl = Repl::new();
 
     repl.run("sqrt(x) = x ^ (1 / 2)").unwrap();
@@ -52,7 +52,7 @@ fn building_sqrt() {
 }
 
 #[test]
-fn building_multiargs() {
+fn multiargs() {
     // implements crazy logic
     // return false if (0, 1)
     // return true if (1, 0)
@@ -69,159 +69,123 @@ fn building_multiargs() {
     expect!(repl, "f(1, 1)", lovm::Value::T(true));
 }
 
-//use crate::{expr::*, repl::*};
-//
-//macro_rules! eq {
-//    ($script:expr, $ex:expr) => {
-//        eq!(Repl::new(), $script, $ex);
-//    };
-//    ($repl:expr, $script:expr, $ex:expr) => {
-//        let result = $repl.run_expr(&$repl.parser.parse($script).unwrap());
-//        assert_eq!(result, $ex);
-//    };
-//}
-//
-//macro_rules! err {
-//    ($script:expr, $msg:expr) => {
-//        err!(Repl::new(), $script, $msg);
-//    };
-//    ($repl:expr, $script:expr, $msg:expr) => {
-//        assert!(
-//            $repl
-//                .run_expr(&$repl.parser.parse($script).unwrap())
-//                .is_err(),
-//            $msg
-//        );
-//    };
-//}
-//
-//fn parse_str(script: &'static str) -> Result<Expr, String> {
-//    match ExprParser::new().parse(script) {
-//        Ok(expr) => Ok(expr),
-//        _ => Err("an error occurred".to_string()),
-//    }
-//}
-//
-//#[test]
-//fn test_numeric() {
-//    // addition
-//    eq!("1 + 1", Ok(Numeric(2.)));
-//    eq!("18 + 18", Ok(Numeric(36.)));
-//
-//    // subtraction
-//    eq!("18 - 18", Ok(Numeric(0.)));
-//
-//    // multiplication
-//    eq!("18 * 18", Ok(Numeric(324.)));
-//
-//    // division
-//    eq!("18 / 18", Ok(Numeric(1.)));
-//
-//    // power
-//    eq!("10 ^ 3", Ok(Numeric(1000.)));
-//
-//    // modulo
-//    eq!("8 % 2", Ok(Numeric(0.)));
-//    eq!("9 % 2", Ok(Numeric(1.)));
-//
-//    // division with zero
-//    err!("18 / 0", "division by zero not allowed");
-//
-//    // different priority
-//    eq!("1 + 4 * 5", Ok(Numeric(21.)));
-//    eq!("(1 + 4) * 5", Ok(Numeric(25.)));
-//    eq!("(1 + 4) * 5 / 2", Ok(Numeric(12.5)));
-//
-//    // addition & subtraction
-//    eq!("1 + 1 - 1 + 1 - 1 + 1 - 1 + 1 - 1", Ok(Numeric(1.)));
-//
-//    // multiplication & division
-//    eq!("2 * 5 / 2 * 5 / 2 * 5", Ok(Numeric(62.5)));
-//
-//    // mixed
-//    eq!("2 + 10 / 2 - 2 * 1 + 1", Ok(Numeric(6.)));
-//    eq!("10 * (2 + 1)", Ok(Numeric(30.)));
-//    eq!("10 * (2 * (2 + 1) - 1) - 1", Ok(Numeric(49.)));
-//
-//    // TODO:
-//    //eq!("10 * [2 + 1]", Ok(Numeric(30.0)));
-//    //eq!("10 * [2*(2 + 1) - 1] - 1", Ok(Numeric(49.0)));
-//    // reducing prefixes
-//    //eq!("--1", Ok(Numeric(1.0)));
-//    // multiplication without parens
-//    //eq!("3*-1", Ok(Numeric(-3.0)));
-//    //eq!("-(1+2)", Ok(Numeric(-3.0)));
-//}
-//
-//#[test]
-//fn parse_errors() {
-//    // two numbers
-//    assert!(
-//        parse_str("10 10").is_err(),
-//        "two numbers not allowed without operator"
-//    );
-//
-//    // two operators
-//    assert!(
-//        parse_str("* /").is_err(),
-//        "two operators not allowed without numbers"
-//    );
-//
-//    // paren nesting incorrect
-//    assert!(
-//        parse_str("10*((2*(2+1)-1)-1").is_err(),
-//        "nesting is not valid"
-//    );
-//    assert!(
-//        parse_str("10*[(2*(2+1)-1]]-1").is_err(),
-//        "nesting is not valid"
-//    );
-//    assert!(
-//        parse_str("10*{(2*(2+1)-1)-1").is_err(),
-//        "nesting is not valid"
-//    );
-//
-//    // empty expression
-//    assert!(parse_str("[]").is_err(), "empty expression is an error");
-//    assert!(parse_str("(())").is_err(), "empty expression is an error");
-//
-//    // assignments
-//    err!("2 = 10", "assignment to constant not allowed");
-//
-//    /*
-//     * TODO: these won't be errors in near future
-//    assert!(
-//        exec_str_pre_num("f(2)=1").is_err(),
-//        "only identifiers allowed in function assignment position"
-//    );
-//
-//    assert!(
-//        exec_str_pre_num("f(y,2)=1").is_err(),
-//        "only identifiers allowed in function assignment position"
-//    );
-//
-//    assert!(
-//        exec_str_pre_num("f(2)=x(1)").is_err(),
-//        "only identifiers allowed in function assignment position"
-//    );
-//    */
-//
-//    // function calls
-//    err!("unknown()", "invalid call");
-//    err!("sqrt()", "invalid call");
-//    err!("sqrt(16, 16)", "invalid call");
-//
-//    // valid identifiers
-//    assert!(parse_str("x").is_ok(), "invalid identifier");
-//    assert!(parse_str("test").is_ok(), "invalid identifier");
-//    assert!(parse_str("test1").is_ok(), "invalid identifier");
-//    assert!(parse_str("1test").is_err(), "invalid identifier");
-//    assert!(parse_str("f'").is_ok(), "invalid identifier");
-//    assert!(parse_str("10").is_ok(), "number is treated as identifier");
-//    assert!(parse_str("empty?").is_ok(), "invalid identifier");
-//    assert!(parse_str("empt?y").is_err(), "invalid identifier");
-//}
-//
+#[test]
+fn numeric() {
+    let mut repl = Repl::new();
+
+    // addition
+    expect!(repl, "1 + 1", lovm::Value::F64(2.));
+    expect!(repl, "18 + 18", lovm::Value::F64(36.));
+
+    // subtraction
+    expect!(repl, "18 - 18", lovm::Value::F64(0.));
+
+    // multiplication
+    expect!(repl, "18 * 18", lovm::Value::F64(324.));
+
+    // division
+    expect!(repl, "18 / 18", lovm::Value::F64(1.));
+
+    // power
+    expect!(repl, "10 ^ 3", lovm::Value::F64(1000.));
+
+    // modulo
+    expect!(repl, "8 % 2", lovm::Value::F64(0.));
+    expect!(repl, "9 % 2", lovm::Value::F64(1.));
+
+    // division with zero
+    //err!("18 / 0", "division by zero not allowed");
+
+    // different priority
+    expect!(repl, "1 + 4 * 5", lovm::Value::F64(21.));
+    expect!(repl, "(1 + 4) * 5", lovm::Value::F64(25.));
+    expect!(repl, "(1 + 4) * 5 / 2", lovm::Value::F64(12.5));
+
+    // addition & subtraction
+    expect!(
+        repl,
+        "1 + 1 - 1 + 1 - 1 + 1 - 1 + 1 - 1",
+        lovm::Value::F64(1.)
+    );
+
+    // multiplication & division
+    expect!(repl, "2 * 5 / 2 * 5 / 2 * 5", lovm::Value::F64(62.5));
+
+    // mixed
+    expect!(repl, "2 + 10 / 2 - 2 * 1 + 1", lovm::Value::F64(6.));
+    expect!(repl, "10 * (2 + 1)", lovm::Value::F64(30.));
+    expect!(repl, "10 * (2 * (2 + 1) - 1) - 1", lovm::Value::F64(49.));
+
+    // TODO:
+    //eq!("10 * [2 + 1]", Ok(Numeric(30.0)));
+    //eq!("10 * [2*(2 + 1) - 1] - 1", Ok(Numeric(49.0)));
+    // reducing prefixes
+    //eq!("--1", Ok(Numeric(1.0)));
+    // multiplication without parens
+    //eq!("3*-1", Ok(Numeric(-3.0)));
+    //eq!("-(1+2)", Ok(Numeric(-3.0)));
+}
+
+#[test]
+fn logical() {
+    let mut repl = Repl::new();
+
+    // constants
+    expect!(repl, "true", lovm::Value::T(true));
+    expect!(repl, "false", lovm::Value::T(false));
+
+    // equal, not equal
+    expect!(repl, "1==1", lovm::Value::T(true));
+    expect!(repl, "1!=1", lovm::Value::T(false));
+
+    // ordering
+    expect!(repl, "20>10", lovm::Value::T(true));
+    expect!(repl, "10<10", lovm::Value::T(false));
+    expect!(repl, "10<1020", lovm::Value::T(true));
+    expect!(repl, "10>1020", lovm::Value::T(false));
+
+    expect!(repl, "10<=10", lovm::Value::T(true));
+    expect!(repl, "10<=11", lovm::Value::T(true));
+    expect!(repl, "10>=5", lovm::Value::T(true));
+    expect!(repl, "10>=10", lovm::Value::T(true));
+
+    // or
+    expect!(repl, "1==1 || 2==2", lovm::Value::T(true));
+    expect!(repl, "1==1 || 2!=2", lovm::Value::T(true));
+    expect!(repl, "1!=1 || 2==2", lovm::Value::T(true));
+    expect!(repl, "1!=1 || 2!=2", lovm::Value::T(false));
+
+    // and
+    expect!(repl, "1==1 && 2==2", lovm::Value::T(true));
+    expect!(repl, "1==1 && 2!=2", lovm::Value::T(false));
+    expect!(repl, "1!=1 && 2==2", lovm::Value::T(false));
+    expect!(repl, "1!=1 && 2!=2", lovm::Value::T(false));
+}
+
+#[test]
+fn context_example() {
+    let mut repl = Repl::new();
+
+    repl.run("even(n) = n % 2 == 0").unwrap();
+    repl.run("odd(n) = even(n) == (1 != 1)").unwrap();
+    repl.run("f(x) = my_const ^ x").unwrap();
+    repl.run("my_const = 10").unwrap();
+    repl.run("x = 2").unwrap();
+
+    expect!(repl, "even(1)", lovm::Value::T(false));
+    expect!(repl, "even(2)", lovm::Value::T(true));
+
+    expect!(repl, "odd(1)", lovm::Value::T(true));
+    expect!(repl, "odd(2)", lovm::Value::T(false));
+
+    expect!(repl, "f(1)", lovm::Value::F64(10.));
+    expect!(repl, "f(x)", lovm::Value::F64(100.));
+
+    repl.run("my_const = 2").unwrap();
+    expect!(repl, "f(1)", lovm::Value::F64(2.));
+    expect!(repl, "f(x)", lovm::Value::F64(4.));
+}
+
 //#[test]
 //fn test_stdlib() {
 //    let mut repl = Repl::with_stdlib();
@@ -259,111 +223,6 @@ fn building_multiargs() {
 //    // empty?
 //    //eq!(repl, "empty?({})", Ok(Logical(true)));
 //    //eq!(repl, "empty?({1})", Ok(Logical(false)));
-//}
-//
-//#[test]
-//fn test_logical() {
-//    // constants
-//    eq!("true", Ok(Logical(true)));
-//    eq!("false", Ok(Logical(false)));
-//
-//    // equal, not equal
-//    eq!("1==1", Ok(Logical(true)));
-//    eq!("1!=1", Ok(Logical(false)));
-//
-//    // ordering
-//    eq!("20>10", Ok(Logical(true)));
-//    eq!("10<10", Ok(Logical(false)));
-//    eq!("10<1020", Ok(Logical(true)));
-//    eq!("10>1020", Ok(Logical(false)));
-//
-//    eq!("10<=10", Ok(Logical(true)));
-//    eq!("10<=11", Ok(Logical(true)));
-//    eq!("10>=5", Ok(Logical(true)));
-//    eq!("10>=10", Ok(Logical(true)));
-//
-//    // or
-//    eq!("1==1 || 2==2", Ok(Logical(true)));
-//    eq!("1==1 || 2!=2", Ok(Logical(true)));
-//    eq!("1!=1 || 2==2", Ok(Logical(true)));
-//    eq!("1!=1 || 2!=2", Ok(Logical(false)));
-//
-//    // and
-//    eq!("1==1 && 2==2", Ok(Logical(true)));
-//    eq!("1==1 && 2!=2", Ok(Logical(false)));
-//    eq!("1!=1 && 2==2", Ok(Logical(false)));
-//    eq!("1!=1 && 2!=2", Ok(Logical(false)));
-//}
-//
-//#[test]
-//fn context_example() {
-//    let mut repl = Repl::with_stdlib();
-//    eq!(repl, "even(n) = n % 2 == 0", Ok(Nil));
-//    eq!(repl, "even(1)", Ok(Logical(false)));
-//    eq!(repl, "even(2)", Ok(Logical(true)));
-//    eq!(repl, "odd(n) = even(n) == (1 != 1)", Ok(Nil));
-//    eq!(repl, "odd(1)", Ok(Logical(true)));
-//    eq!(repl, "odd(2)", Ok(Logical(false)));
-//
-//    eq!(repl, "my_const = 10", Ok(Nil));
-//    eq!(repl, "x = 2", Ok(Nil));
-//    eq!(repl, "f(x) = my_const ^ x", Ok(Nil));
-//    eq!(repl, "f(1)", Ok(Numeric(10.)));
-//    eq!(repl, "f(x)", Ok(Numeric(100.)));
-//    eq!(repl, "my_const = 2", Ok(Nil));
-//    eq!(repl, "f(1)", Ok(Numeric(2.)));
-//    eq!(repl, "f(x)", Ok(Numeric(4.)));
-//}
-//
-//#[test]
-//fn context_recursion() {
-//    let mut repl = Repl::with_stdlib();
-//    eq!(repl, "f(0) = 0", Ok(Nil));
-//    eq!(repl, "f(1) = 1", Ok(Nil));
-//    eq!(repl, "f(x) = f(x - 1) + f(x - 2)", Ok(Nil));
-//
-//    eq!(repl, "f(0)", Ok(Numeric(0.)));
-//    eq!(repl, "f(1)", Ok(Numeric(1.)));
-//    eq!(repl, "f(2)", Ok(Numeric(1.)));
-//    eq!(repl, "f(3)", Ok(Numeric(2.)));
-//    eq!(repl, "f(4)", Ok(Numeric(3.)));
-//    eq!(repl, "f(5)", Ok(Numeric(5.)));
-//    eq!(repl, "f(9)", Ok(Numeric(34.)));
-//}
-//
-//#[test]
-//fn test_optimize() {
-//    use crate::ast::Expr::*;
-//    let repl = Repl::with_stdlib();
-//    let parse_optimized = |repl: &Repl, s: &str| -> Option<Expr> {
-//        match ExprParser::new().parse(s) {
-//            Ok(mut program) => {
-//                repl.optimize(&mut program).unwrap();
-//                Some(program)
-//            }
-//            _ => unimplemented!(),
-//        }
-//    };
-//
-//    match parse_optimized(&repl, "1 + 2 + 3") {
-//        Some(Value(Numeric(n))) if n == 6. => {}
-//        _ => panic!("optimization wrong"),
-//    }
-//
-//    match parse_optimized(&repl, "1 != 1 && 1 == 1") {
-//        Some(Value(Logical(false))) => {}
-//        _ => panic!("optimization wrong"),
-//    }
-//
-//    match parse_optimized(&repl, "1 == 1 || 1 != 1") {
-//        Some(Value(Logical(true))) => {}
-//        _ => panic!("optimization wrong"),
-//    }
-//
-//    match parse_optimized(&repl, "x * 0") {
-//        Some(Value(Numeric(n))) if n == 0. => {}
-//        _ => panic!("optimization wrong"),
-//    }
 //}
 //
 //#[test]
